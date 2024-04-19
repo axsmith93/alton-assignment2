00c702140385fa7ac3dc67f2e9e78e84fcc2ace9
axsmith93 <altonxsmith93@yahoo.com>
2024-04-19 14:38:17 -0400

    V2 changes

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -3,7 +3,9 @@ package com.coderscampus;
 public class Assignment2Application {
 
 	public static void main(String[] args) {
-		
+		// generate a random number
+		//collect user input
+		//etc
 
 	}
 
