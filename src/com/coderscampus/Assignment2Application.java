ef309c427d2aad10e5bbb2c7d954752dd817c0cf
axsmith93 <altonxsmith93@yahoo.com>
2024-04-21 20:02:31 -0400

    figured out how to stop guessing at 5

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -1,12 +1,43 @@
 package com.coderscampus;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Assignment2Application {
 
 	public static void main(String[] args) {
-		// generate a random number
+		//generate a random number
 		//collect user input
-		//etc
+		Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
-	}
+        int theRandomNumber = random.nextInt(100) + 1;
+        int maxTries = 0;
+        
+        System.out.println("Welcome to Guess the Number! "
+        		+ "Can you guess the number between 1 and 100?");
+        
 
-}
+        while (maxTries < 5) {
+            System.out.print("Pick a number between 1 and 100: ");
+            String input = scanner.nextLine();
+            maxTries++;
+            
+            int guess = Integer.parseInt(input);
+       
+            if (guess < 1 || guess > 100) {
+                System.out.println("Your guess is not between 1 and 100,"
+                		+ " please try again.");
+                
+            }
+            if (guess < theRandomNumber) {
+                System.out.println("Please pick a higher number.");
+            } else if (guess > theRandomNumber) {
+                System.out.println("Please pick a lower number.");
+            } else {
+                System.out.println("Congratulations! You win!");
+            }
+            }
+                System.out.println("You lose! The number to guess was: " + theRandomNumber);
+        }
+    }
